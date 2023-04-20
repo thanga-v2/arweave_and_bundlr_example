3811e3dd19db00c80238e0479e5f099787b57a85
thangasblockchain <thangarajm@icloud.com>
2023-04-20 16:16:46 +0530

    using state to store bundlr instance

--- arweave_bundlr/pages/_app.js
+++ arweave_bundlr/pages/_app.js
@@ -3,8 +3,14 @@ import '@/styles/globals.css'
 import { providers,utils } from 'ethers';
 // importing the web bundlr to start interacting
 import { WebBundlr } from '@bundlr-network/client';
+// using state and ref, we create a refernce to bundlr
+import { useState,useRef } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+      // using state to store bundlr instance
+  const[bundlrInstance,setbundlrInstance] = useState();
+
   // creating initialize for bundlr
   async function initialize(){
     console.log("initialize function to init bundlr");
@@ -14,10 +20,18 @@ export default function App({ Component, pageProps }) {
     const provider = new providers.Web3Provider(window.ethereum);
     console.log("provider status -",provider);
     await provider._ready()
-
     const bundlr = new WebBundlr("https://node1.bundlr.network","matic",provider);
     console.log("bundlr status",bundlr);
     await bundlr.ready()
+    // using bundlr instnace state to store the state
+    setbundlrInstance(bundlr);
+    console.log("Is the bundlr state has been set ? ////",bundlrInstance);
+  }
+
+  // creating a function to fetch balance
+  // but we need to reference bundlr before doing so. lets set a state ref
+  async function fetchbalance(){
+
   }
   return (
     <div>
